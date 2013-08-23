ba89432f8a359685994324a67ba5676690f048f1
harirordeveloper <harirordeveloper@gmail.com>
2013-08-23 20:30:43 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Satish::Application.config.secret_token = 'b81683056d042f66c4eed88c02738270b21e7a0da63594f640a1f81cc972a378d348b85cf4340cd3f315790308215d4f71845de63a09dd175eb07b95231e9f52'
